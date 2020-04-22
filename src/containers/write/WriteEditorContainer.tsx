2e6be5d374b6121f722e2a3f773f569faa0ad820
Genie Jung <genie.jung@kor.mvision.lan>
2020-04-22 18:28:31 +0900

    Add css code and Move WriteButton into WriteEditorContainer.

--- src/containers/write/WriteEditorContainer.tsx
+++ src/containers/write/WriteEditorContainer.tsx
@@ -1,10 +1,13 @@
-import React, { useMemo } from 'react';
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import { useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../modules';
 import { changeMarkdown, changeTitle } from '../../modules/write'
 import WriteTitleTextArea from '../../components/write/WriteTitleTextArea';
 import MarkdownEditor from '../../components/write/MarkdownEditor';
 import { bindActionCreators } from 'redux';
+import WriteButtonGroupContainer from "./WriteButtonGroupContainer";
 
 function WriteEditorContainer(){
     const { title, body } = useSelector((state: RootState) => state.write);
@@ -17,11 +20,19 @@ function WriteEditorContainer(){
     ,[dispatch]);
 
     return (
-        <div>
+        <div css={style}>
             <WriteTitleTextArea onTitleChange={actionCreators.changeTitle} title={title}/>
             <MarkdownEditor markdown={body} onChangeMarkdown={actionCreators.changeMarkdown}/>
+            <WriteButtonGroupContainer />
         </div>
     );
 }
+
+const style = css`
+    width:100%;
+    height:100%;
+    display: flex;
+    flex-direction: column;
+`;
 
 export default WriteEditorContainer;
--- src/pages/WritePage.tsx
+++ src/pages/WritePage.tsx
@@ -1,14 +1,10 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 import WriteEditorContainer from '../containers/write/WriteEditorContainer';
-import WriteButtonGroupContainer from '../containers/write/WriteButtonGroupContainer';
 
 const WritePage = () => {
     return(
-        <div>
             <WriteEditorContainer />
-            <WriteButtonGroupContainer />
-        </div>
     );
 }
 
