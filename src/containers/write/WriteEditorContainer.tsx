d4d5881630eee1c07e8153dd01f080d22847a282
unknown <hee.1.63m@gmail.com>
2020-04-27 02:16:44 +0900

    Clean code line.

--- src/containers/write/WriteEditorContainer.tsx
+++ src/containers/write/WriteEditorContainer.tsx
@@ -1,38 +1,50 @@
 /** @jsx jsx */
 import { jsx, css } from "@emotion/core";
-import { useMemo } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from '../../modules';
-import { changeMarkdown, changeTitle } from '../../modules/write'
-import WriteTitleTextArea from '../../components/write/WriteTitleTextArea';
-import MarkdownEditor from '../../components/write/MarkdownEditor';
-import { bindActionCreators } from 'redux';
+import { useMemo } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../../modules";
+import { changeMarkdown, changeTitle } from "../../modules/write";
+import WriteTitleTextArea from "../../components/write/WriteTitleTextArea";
+import MarkdownEditor from "../../components/write/MarkdownEditor";
+import { bindActionCreators } from "redux";
 import WriteButtonGroupContainer from "./WriteButtonGroupContainer";
+import MarkdownViewer from "../../components/post/MarkdownViewer";
 
-function WriteEditorContainer(){
-    const { title, body } = useSelector((state: RootState) => state.write);
-    const dispatch = useDispatch();
-    const actionCreators = useMemo(
-        () => bindActionCreators({
-            changeMarkdown,
-            changeTitle
-        }, dispatch)
-    ,[dispatch]);
+function WriteEditorContainer() {
+  const { title, body } = useSelector((state: RootState) => state.write);
+  const dispatch = useDispatch();
+  const actionCreators = useMemo(
+    () =>
+      bindActionCreators(
+        {
+          changeMarkdown,
+          changeTitle,
+        },
+        dispatch
+      ),
+    [dispatch]
+  );
 
-    return (
-        <div css={style}>
-            <WriteTitleTextArea onTitleChange={actionCreators.changeTitle} title={title}/>
-            <MarkdownEditor markdown={body} onChangeMarkdown={actionCreators.changeMarkdown}/>
-            <WriteButtonGroupContainer />
-        </div>
-    );
+  return (
+    <div css={style}>
+      <WriteTitleTextArea
+        onTitleChange={actionCreators.changeTitle}
+        title={title}
+      />
+      <MarkdownEditor
+        markdown={body}
+        onChangeMarkdown={actionCreators.changeMarkdown}
+      />
+      <WriteButtonGroupContainer />
+    </div>
+  );
 }
 
 const style = css`
-    width:100%;
-    height:100%;
-    display: flex;
-    flex-direction: column;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 `;
 
 export default WriteEditorContainer;
